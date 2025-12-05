11c45618eb2686bbff712984b49b967ca3ec3098
Garik_18 <garikarshakyam27@gmail.com>
2025-12-05 20:04:30 +0300

    add

---
+++ assets/js/script.js
@@ -0,0 +1,100 @@
+$(document).ready(function () {
+    $('.open-menu').on('click', function () {
+        $(this).toggleClass('close-menu');
+        if ($(this).hasClass('close-menu')) {
+            $('.menu-content').addClass('transition-menu');
+            $('body').addClass('body_fix');
+        } else {
+            $('.menu-content').addClass('menu-width');
+            $('body').removeClass('body_fix');
+            $('.menu-content').removeClass('transition-menu');
+        }
+    });
+    $('.nav-menu a').on('click', function () {
+        $('.menu-content').addClass('menu-width');
+        $('body').removeClass('body_fix');
+        $('.menu-content').removeClass('transition-menu');
+        $('.open-menu').removeClass('close-menu');
+    })
+});
+
+
+
+
+
+$(function () {
+    let Accordion = function (el, multiple) {
+        this.el = el || {};
+        this.multiple = multiple || false;
+        let links = this.el.find('.link');
+        links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+    };
+
+    Accordion.prototype.dropdown = function (e) {
+        let $el = e.data.el;
+        $this = $(this),
+            $next = $this.next();
+        $next.slideToggle();
+        if (!e.data.multiple) {
+
+            $el.find('.submenu-faq').not($next).slideUp();
+        }
+        if (!$this.hasClass('open')) {
+            $('.link').each(function () {
+                $(this).removeClass('open')
+            })
+            $this.addClass('open')
+        } else {
+            $this.removeClass('open')
+        }
+    }
+
+    let accordion = new Accordion($('#accordion'), false);
+});
+
+
+$('.menu-scroll a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+        let $target = $(this.hash);
+        $target = $target.length && $target
+            || $('[name=' + this.hash.slice(1) +']');
+        if ($target.length) {
+            let targetOffset = $target.offset().top-130;
+            $('html,body')
+                .animate({scrollTop: targetOffset}, 1000);
+            return false;
+        }
+    }
+});
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector(".header");
+    const target = document.querySelector(".section-work-pdf");
+
+    if (!header || !target) return;
+
+    const headerHeight = header.offsetHeight;
+
+    function updateHeaderColor() {
+        const rect = target.getBoundingClientRect();
+
+        // Есть ли пересечение блока с зоной шапки
+        const isOverBlock = rect.top <= headerHeight && rect.bottom >= 0;
+
+        if (isOverBlock) {
+            header.classList.add("header-dark");
+        } else {
+            header.classList.remove("header-dark");
+        }
+    }
+
+    window.addEventListener("scroll", updateHeaderColor, { passive: true });
+    window.addEventListener("resize", updateHeaderColor);
+    updateHeaderColor(); // на всякий случай при загрузке
+});
